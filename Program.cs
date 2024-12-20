0514177bbfd59fdeaf9ac4d38d8e86553c819ca3
Thoriangel321 <93290915+Thoriangel321@users.noreply.github.com>
2024-12-20 21:54:27 +0200

    Added Files

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿namespace AlphabetChecker
+{
+    internal class Program
+    {
+
+        static bool IsDigit(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+
+        static bool IsLetterInFirstRange(char c)
+        {
+            return c >= 'A' && c <= 'E';
+        }
+
+        static bool IsLetterInSecondRange(char c)
+        {
+            return c >= 'F' && c <= 'Z';
+        }
+
+
+        static bool IsLineValid(string? str)
+        {
+            int StringLength = str.Length;
+
+            if (String.IsNullOrEmpty(str) || str[0] != 'A')
+            {
+                return false;
+            }
+
+            int i;
+
+            for(i = 0; i < StringLength; i++)
+            {
+                char charr = str[i];
+                if (charr == '_')
+                {
+                    if (str[i] == '_')
+                    {
+                        if (IsLetterInFirstRange(str[i - 1]) && IsLetterInSecondRange(str[i + 1]) || IsDigit(str[i + 1]))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (str[i] != '_')
+                    {
+                        return true;
+                    }
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+
+
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string UserInputString;
+            Console.Write("Write a phrase: ");
+            UserInputString = Console.ReadLine();
+            Console.WriteLine();
+            if(IsLineValid(UserInputString))
+            {
+                Console.WriteLine("Line is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Line is Invalid");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AlphabetChecker
  
+goofy app I made to train my C# skills. The operating principle is that you input a string, first part of which must start with letters in range from 'A' to 'E', then it can have (it can, but it's not necessary) an underscore sign '_' after this only letters in range from 'F' to 'Z' or only digits between '0' and '9'
+
+firstly written in C++, then moved to C#. Made within 15 minutes. Why the hell C# can't return only one type of value ? :x 12.20.24
